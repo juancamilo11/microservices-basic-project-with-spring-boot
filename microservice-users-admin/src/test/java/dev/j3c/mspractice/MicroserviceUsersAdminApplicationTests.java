356b97a511c562ce95c568d8df90fe0e28117176
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2022-01-21 22:20:17 -0500

    Added basic config

---
+++ microservice-users-admin/src/test/java/dev/j3c/mspractice/MicroserviceUsersAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.j3c.mspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceUsersAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
